efb7fb615afe3bbc1e13cc7c2a2c84ede2d187ab
Sergei Churbanov <csm@kontur.ru>
2019-09-10 14:20:08 +0500

    ConfigureAwait

--- Vostok.ServiceDiscovery.Extensions/IServiceDiscoveryManagerExtensions.cs
+++ Vostok.ServiceDiscovery.Extensions/IServiceDiscoveryManagerExtensions.cs
@@ -11,41 +11,44 @@ namespace Vostok.ServiceDiscovery.Extensions
         public static async Task<bool> AddToBlacklist(this IServiceDiscoveryManager serviceDiscoveryManager, string environment, string application, params Uri[] replicaUri)
         {
             return await serviceDiscoveryManager.TryUpdateApplicationPropertiesAsync(
-                environment,
-                application,
-                properties =>
-                {
-                    var blacklist = new HashSet<Uri>(properties.GetBlacklist());
-                    if (blacklist.IsSupersetOf(replicaUri))
-                        return properties;
+                    environment,
+                    application,
+                    properties =>
+                    {
+                        var blacklist = new HashSet<Uri>(properties.GetBlacklist());
+                        if (blacklist.IsSupersetOf(replicaUri))
+                            return properties;
 
-                    var newBlackList = blacklist.Concat(replicaUri);
-                    return properties.SetBlacklist(newBlackList);
-                });
+                        var newBlackList = blacklist.Concat(replicaUri);
+                        return properties.SetBlacklist(newBlackList);
+                    })
+                .ConfigureAwait(false);
         }
 
         public static async Task<bool> RemoveFromBlacklist(this IServiceDiscoveryManager serviceDiscoveryManager, string environment, string application, params Uri[] replicaUri)
         {
             return await serviceDiscoveryManager.TryUpdateApplicationPropertiesAsync(
-                environment,
-                application,
-                properties =>
-                {
-                    var blacklist = new HashSet<Uri>(properties.GetBlacklist());
-                    if (!replicaUri.Intersect(blacklist).Any())
-                        return properties;
+                    environment,
+                    application,
+                    properties =>
+                    {
+                        var blacklist = new HashSet<Uri>(properties.GetBlacklist());
+                        if (!replicaUri.Intersect(blacklist).Any())
+                            return properties;
 
-                    blacklist.ExceptWith(replicaUri);
-                    return properties.SetBlacklist(blacklist);
-                });
+                        blacklist.ExceptWith(replicaUri);
+                        return properties.SetBlacklist(blacklist);
+                    })
+                .ConfigureAwait(false);
         }
 
         public static async Task<bool> SetExternalUrl(this IServiceDiscoveryManager serviceDiscoveryManager, string environment, string application, Uri externalUrl)
         {
             return await serviceDiscoveryManager.TryUpdateApplicationPropertiesAsync(
-                environment,
-                application,
-                properties => properties.SetExternalUrl(externalUrl));
-        } 
+                    environment,
+                    application,
+                    properties => properties.SetExternalUrl(externalUrl))
+                .ConfigureAwait(false);
+        }
     }
 }
