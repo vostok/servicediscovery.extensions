0d515e42106caaee9940e8884553e53de093523f
iloktionov <iloktionov@skbkontur.ru>
2020-02-26 13:19:57 +0500

    ReplicaComparer is now public. It also considers only the first segment of URL hosts from now on.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,14 @@
-## 0.1.3 (12-09-2019)
+## 0.1.4 (26-02-2020):
+
+ReplicaComparer is now public. It also considers only the first segment of URL hosts from now on.
+
+## 0.1.3 (12-09-2019):
 
 Added extensions to add replicas to application blacklist (see `AddToBlacklistAsync()`).
 Added extensions to remove replicas from application blacklist (see `RemoveFromBlacklistAsync()`).
 Added extensions to set application external url (see `SetExternalUrlAsync()`).
 
-## 0.1.1 (20-07-2019)
+## 0.1.1 (20-07-2019):
 
 Added an extension to extract replica weights from topology properties (see `GetReplicaWeights()`).
 
---
+++ Vostok.ServiceDiscovery.Extensions.Tests/Helpers/ReplicaComparer_Tests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using Vostok.ServiceDiscovery.Extensions.Helpers;
+
+namespace Vostok.ServiceDiscovery.Extensions.Tests.Helpers
+{
+    [TestFixture]
+    internal class ReplicaComparer_Tests
+    {
+        [Test]
+        public void Should_not_compare_paths()
+            => ShouldBeEqual("http://replica/foo", "http://replica/bar");
+
+        [Test]
+        public void Should_not_compare_query_parameters()
+            => ShouldBeEqual("http://replica/foo?a=b", "http://replica/bar?c=d");
+
+        [Test]
+        public void Should_be_case_insensitive()
+            => ShouldBeEqual("http://replica/", "http://REPLICA/");
+
+        [Test]
+        public void Should_ignore_domain_names()
+            => ShouldBeEqual("http://replica.domain1/", "http://replica.domain2.com/");
+
+        [Test]
+        public void Should_differentiate_replicas_by_host_name()
+        {
+            ShouldBeDifferent("http://replica", "http://rep");
+            ShouldBeDifferent("http://replica1", "http://replica2");
+        }
+
+        [Test]
+        public void Should_differentiate_replicas_by_port()
+            => ShouldBeDifferent("http://replica:81", "http://replica:82");
+
+        [Test]
+        public void Should_recognize_equal_ip_addresses()
+            => ShouldBeEqual("http://1.2.3.4/", "http://1.2.3.4/");
+
+        [Test]
+        public void Should_recognize_different_ip_addresses()
+            => ShouldBeDifferent("http://1.2.3.4/", "http://1.2.5.4/");
+
+        private static void ShouldBeEqual(string replica1, string replica2)
+        {
+            Check(replica1, replica2, true);
+            Check(replica2, replica1, true);
+        }
+
+        private static void ShouldBeDifferent(string replica1, string replica2)
+        {
+            Check(replica1, replica2, false);
+            Check(replica2, replica1, false);
+        }
+
+        private static void Check(string replica1, string replica2, bool shouldBeEqual)
+        {
+            var url1 = new Uri(replica1);
+            var url2 = new Uri(replica2);
+
+            ReplicaComparer.Instance.Equals(url1, url2).Should().Be(shouldBeEqual);
+
+            var hash1 = ReplicaComparer.Instance.GetHashCode(url1);
+            var hash2 = ReplicaComparer.Instance.GetHashCode(url2);
+
+            (hash1 == hash2).Should().Be(shouldBeEqual);
+        }
+    }
+}
