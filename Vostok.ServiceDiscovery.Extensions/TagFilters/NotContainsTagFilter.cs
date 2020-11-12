3e8b5ef3b7d85209feb33e08361ab36a49acf120
Borovsky Arkady <borovskyav@list.ru>
2020-11-12 19:31:01 +0500

    Add tag filter abstraction and realizations

---
+++ Vostok.ServiceDiscovery.Extensions/TagFilters/NotContainsTagFilter.cs
@@ -0,0 +1,36 @@
+using System;
+using JetBrains.Annotations;
+using Vostok.ServiceDiscovery.Abstractions.Models;
+
+namespace Vostok.ServiceDiscovery.Extensions.TagFilters
+{
+    /// <summary>
+    /// An implementation of <see cref="ITagFilter" /> that implements simple "not contains" expression.
+    /// </summary>
+    [PublicAPI]
+    public class NotContainsTagFilter : ITagFilter
+    {
+        public NotContainsTagFilter([NotNull] string key)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+                throw new ArgumentOutOfRangeException(nameof(key));
+
+            Key = key.Trim();
+        }
+
+        [NotNull]
+        public string Key { get; }
+
+        /// <summary>
+        /// <para> Returns true if <see cref="NotContainsTagFilter.Key" /> is not present in <paramref name="collection" /> or
+        /// <paramref
+        ///     name="collection" />
+        /// is null. </para>
+        /// </summary>
+        public bool Matches(TagCollection collection)
+            => collection == null
+               || !collection.ContainsKey(Key);
+
+        public override string ToString() => "!" + Key;
+    }
+}
