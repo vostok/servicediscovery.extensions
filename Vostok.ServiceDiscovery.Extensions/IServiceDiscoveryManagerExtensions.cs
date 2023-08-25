bcf9e78fe6d83db7c411f9eb233d31012b2e6c38
Vladislav Kulbachniy <kulbachnyi.v@skbkontur.ru>
2023-08-25 12:26:32 +0500

    refactoring  clear ephemeral and persistent replica tags async method

--- Vostok.ServiceDiscovery.Extensions/IServiceDiscoveryManagerExtensions.cs
+++ Vostok.ServiceDiscovery.Extensions/IServiceDiscoveryManagerExtensions.cs
@@ -51,15 +51,33 @@ namespace Vostok.ServiceDiscovery.Extensions
         /// <para>This method modifies <see cref="ReplicaTagKind.Persistent"/> tags.</para>
         /// <para>See <see cref="ReplicaTagKind"/> for more information about different tags kinds.</para>
         /// </summary>
+        [Obsolete("This method is obsolete, because it's signature is incorrect. Use this method without tagKeysToRemove instead.", false)]
         public static async Task<bool> ClearPersistentReplicaTagsAsync(this IServiceDiscoveryManager serviceDiscoveryManager, string environment, string application, string replicaName, IEnumerable<string> tagKeysToRemove)
             => await serviceDiscoveryManager.ClearReplicaTagsAsync(environment, application, replicaName, ReplicaTagKind.Persistent).ConfigureAwait(false);
 
         /// <summary>
         /// <para><inheritdoc cref="ClearReplicaTagsAsync"/></para>
+        /// <para>This method modifies <see cref="ReplicaTagKind.Persistent"/> tags.</para>
+        /// <para>See <see cref="ReplicaTagKind"/> for more information about different tags kinds.</para>
+        /// </summary>
+        public static async Task<bool> ClearPersistentReplicaTagsAsync(this IServiceDiscoveryManager serviceDiscoveryManager, string environment, string application, string replicaName)
+            => await serviceDiscoveryManager.ClearReplicaTagsAsync(environment, application, replicaName, ReplicaTagKind.Persistent).ConfigureAwait(false);
+        
+        /// <summary>
+        /// <para><inheritdoc cref="ClearReplicaTagsAsync"/></para>
         /// <para>This method modifies <see cref="ReplicaTagKind.Ephemeral"/> tags.</para>
         /// <para>See <see cref="ReplicaTagKind"/> for more information about different tags kinds.</para>
         /// </summary>
+        [Obsolete("This method is obsolete, because it's signature is incorrect. Use this method without tagKeysToRemove instead.", false)]
         public static async Task<bool> ClearEphemeralReplicaTagsAsync(this IServiceDiscoveryManager serviceDiscoveryManager, string environment, string application, string replicaName, IEnumerable<string> tagKeysToRemove)
+            => await serviceDiscoveryManager.ClearReplicaTagsAsync(environment, application, replicaName, ReplicaTagKind.Ephemeral).ConfigureAwait(false);
+        
+        /// <summary>
+        /// <para><inheritdoc cref="ClearReplicaTagsAsync"/></para>
+        /// <para>This method modifies <see cref="ReplicaTagKind.Ephemeral"/> tags.</para>
+        /// <para>See <see cref="ReplicaTagKind"/> for more information about different tags kinds.</para>
+        /// </summary>
+        public static async Task<bool> ClearEphemeralReplicaTagsAsync(this IServiceDiscoveryManager serviceDiscoveryManager, string environment, string application, string replicaName)
             => await serviceDiscoveryManager.ClearReplicaTagsAsync(environment, application, replicaName, ReplicaTagKind.Ephemeral).ConfigureAwait(false);
 
         /// <summary>
